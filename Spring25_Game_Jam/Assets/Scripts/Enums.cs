0af27182dff42196790382b73b97cffeaaa9bb93
KayneTheBurr <kainejohnson63@gmail.com>
2025-03-21 12:14:05 -0700

    Making the base scripts and player

---
+++ Spring25_Game_Jam/Assets/Scripts/Enums.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Enums : MonoBehaviour
+{
+}
+public enum PlayerState
+{
+    Bouba,
+    Kikki,
+    None
+}
---
+++ Spring25_Game_Jam/Assets/Scripts/Player/PlayerCombatManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PlayerCombatManager : MonoBehaviour
+{
+   
+}
